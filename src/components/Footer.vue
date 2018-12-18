d844b45f3667fc23239d053c6dc40c99d07dd6be
Irina Lepekhina <irina.lepekhina95@gmail.com>
2018-12-18 22:55:46 +0200

    Formatted and stylized menu in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,31 +10,50 @@
     </div>
     <div class="footer__contacts">
       <div class="contacts__phone">
-        <img src="@/assets/icon-phone.svg" alt="phone"> 
-        <span>Phone: <a href="callto:+15431234567">+1-543-123-4567</a></span>
+        <img src="@/assets/icon-phone.svg" alt="phone">
+        <span>Phone:
+          <a href="callto:+15431234567">+1-543-123-4567</a>
+        </span>
       </div>
       <div class="contacts__email">
         <img src="@/assets/icon-email.svg" alt="email">
         <a href="mailto:example@fylo.com">example@fylo.com</a>
       </div>
     </div>
-    Fylo
-    Desktop App
-    Mobile App
-    Pricing
-    Feature
-    Company
-    About Us
-    Jobs
-    Press
-    Blog
-    Support
-    Contact Us
-    Terms
-    Privacy
+    <div class="footer__nav">
+      <div class="nav__left">
+        <h2>Fylo</h2>
+        <ul>
+          <li>Desktop App</li>
+          <li>Mobile App</li>
+          <li>Pricing</li>
+          <li>Feature</li>
+        </ul>
+      </div>
+      <div class="nav__middle">
+        <h2>Company</h2>
+        <ul>
+          <li>About Us</li>
+          <li>Jobs</li>
+          <li>Press</li>
+          <li>Blog</li>
+        </ul>
+      </div>
+      <div class="nav__right">
+        <h2>Support</h2>
+        <ul>
+          <li>Contact Us</li>
+          <li>Terms</li>
+          <li>Privacy</li>
+        </ul>
+      </div>
+    </div>
     <p class="attribution">
       This page started life as a challenge on
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a> 🎉
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+      >Frontend Mentor</a> 🎉
     </p>
   </footer>
 </template>
@@ -92,13 +111,31 @@
   color: $white;
 }
 
-.contacts__phone, .contacts__email {
+.contacts__phone,
+.contacts__email {
   display: flex;
   align-items: center;
   margin: 0.75rem 0;
 }
 
-.contacts__phone img, .contacts__email img {
+.contacts__phone img,
+.contacts__email img {
   padding-right: 1.5rem;
+}
+
+.footer__nav {
+  padding: 6rem 0;
+}
+
+.footer__nav h2 {
+  color: $white;
+}
+
+.footer__nav ul {
+  padding: 1.5rem 0 4rem 0;
+}
+
+.footer__nav ul li {
+  line-height: 2.5;
 }
 </style>
